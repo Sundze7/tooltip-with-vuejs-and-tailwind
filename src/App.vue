7b68b1ac9eb9f1b9ae8e03a2d15638c99c13efd0
Sundze <keyonmwesundze@mail.com>
2022-12-03 11:14:04 +0100

    added tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,40 @@
 <template>
-  <tool-tip content="my tooltip" placement="bottom">
-    <button
-      class="bg-blue-500 text-white text-sm font-medium rounded-md leading-none px-3 py-2"
-    >
-      click me
-    </button>
-  </tool-tip>
+  <div class="flex items-center justify-center mt-16">
+    <tool-tip content="my tooltip" placement="top">
+      <button
+        class="bg-red-500 text-white text-sm font-medium rounded-md leading-none px-3 py-2"
+      >
+        Top
+      </button>
+    </tool-tip>
+  </div>
+  <div class="flex items-center justify-center m-8">
+    <tool-tip content="my tooltip" placement="right">
+      <button
+        class="bg-blue-500 text-white text-sm font-medium rounded-md leading-none px-3 py-2"
+      >
+        Right
+      </button>
+    </tool-tip>
+  </div>
+  <div class="flex items-center justify-center mb-8">
+    <tool-tip content="my tooltip" placement="left">
+      <button
+        class="bg-green-500 text-white text-sm font-medium rounded-md leading-none px-3 py-2"
+      >
+        Left
+      </button>
+    </tool-tip>
+  </div>
+  <div class="flex items-center justify-center mb-8">
+    <tool-tip content="my tooltip" placement="bottom">
+      <button
+        class="bg-pink-500 text-white text-sm font-medium rounded-md leading-none px-3 py-2"
+      >
+        Bottom
+      </button>
+    </tool-tip>
+  </div>
 </template>
 
 <script>
